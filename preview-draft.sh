1dd3673bbcd0127fb2975839312b83fe7af3225d
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-02-25 15:43:22 +0000

    Publish.

--- preview-draft.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find drafts -name "*$1*.markdown" | while read -r line ; do
-  filename=$(echo "$line" | cut -c8-)
-  mv "$line" "posts/$(date +%Y-%m-%d)-$filename"
-done
--- store-draft.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find posts/ -name "*$1*.markdown" | while read -r line ; do
-  filename=$(echo "$line" | cut -c19-)
-  mv "$line" "drafts/$filename"
-done
-
